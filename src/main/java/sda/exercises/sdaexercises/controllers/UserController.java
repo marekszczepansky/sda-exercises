92a2865b6cb466855695a751d49105b6e4b222b1
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-02-27 14:00:27 +0100

    put mapping added delete mapping added

--- src/main/java/sda/exercises/sdaexercises/controllers/UserController.java
+++ src/main/java/sda/exercises/sdaexercises/controllers/UserController.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * localhost:8080/users - lista użytkowników
- * localhost:8080/users/1 - użytkownik o id 1
+ * localhost:8080/users - lista użytkowników, POST
+ * localhost:8080/users/1 - użytkownik o id 1 - GET, PUT, DELETE
  */
 @RestController
 @RequestMapping("users")
@@ -17,19 +17,31 @@ public class UserController {
     private List<User> userList = new ArrayList<>();
 
     @GetMapping
-    public List<User> getUsers(){
+    public List<User> getUsers() {
         return userList;
     }
 
     @GetMapping("{id}")
-    public User getUser(@PathVariable Integer id){
+    public User getUser(@PathVariable Integer id) {
         return userList.get(id);
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user){
+    public User createUser(@RequestBody User user) {
         user.setId(userList.size());
         userList.add(user);
+        return user;
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userList.remove(id);
+    }
+
+    @PutMapping("{id}")
+    public User updateUser(@PathVariable Integer id, @RequestBody User user) {
+        user.setId(id);
+        userList.set(id, user);
         return user;
     }
 
