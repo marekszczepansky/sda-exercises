3f24a489732d81a563c42b8f845ec1205ebde58d
Marek Szczepanski <mszczepanski2@atlassian.com>
2021-02-27 10:51:06 +0100

    Initial commit

---
+++ src/main/java/sda/exercises/sdaexercises/SdaExercisesApplication.java
@@ -0,0 +1,13 @@
+package sda.exercises.sdaexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sda/exercises/sdaexercises/SdaExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.exercises.sdaexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
